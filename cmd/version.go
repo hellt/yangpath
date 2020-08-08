82fb3f70bccc5ce7b63dcbd064bc14f897afe6a2
dodin.roman@gmail.com <dodin.roman@gmail.com>
2020-08-08 08:30:33 +0200

    added version command

---
+++ cmd/version.go
@@ -0,0 +1,45 @@
+// Copyright © 2020 Roman Dodin <dodin.roman@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "show yangpath version",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("version : %s\n", version)
+		fmt.Printf(" commit : %s\n", commit)
+		fmt.Printf("   date : %s\n", date)
+		fmt.Printf(" source : https://github.com/hellt/yangpath\n")
+		fmt.Printf("   docs : https://yangpath.netdevops.me\n")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
